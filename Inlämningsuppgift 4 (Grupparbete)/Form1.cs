8b11089a6298fc601062003c4d4f45238d0295b5
simtheking <simpan.andersson93@gmail.com>
2023-10-04 13:42:46 +0200

    test123
    test123

--- Inlämningsuppgift 4 (Grupparbete)/Form1.cs
+++ Inlämningsuppgift 4 (Grupparbete)/Form1.cs
@@ -6,5 +6,10 @@ namespace Inlämningsuppgift_4__Grupparbete_
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            simons test 1
+        }
     }
 }
