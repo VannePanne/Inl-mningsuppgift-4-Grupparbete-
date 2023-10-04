56dc45083443850fd6cbe5dd710a9643551a51b3
najouh <najahkhaderr@gmail.com>
2023-10-04 14:40:30 +0200

    comment

--- Inlämningsuppgift 4 (Grupparbete)/Form1.cs
+++ Inlämningsuppgift 4 (Grupparbete)/Form1.cs
@@ -10,6 +10,8 @@ namespace Inlämningsuppgift_4__Grupparbete_
         private void Form1_Load(object sender, EventArgs e)
         {
             textBox1.Text = "simon123";
+            // addes this information
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
