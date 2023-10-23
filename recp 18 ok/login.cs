f3e003576d3d8190dc3c2f2d37e19b6fcfd01043
najouh <najahkhaderr@gmail.com>
2023-10-23 17:34:37 +0200

    Testade att göra inloggning objekt oriented

--- recp 18 ok/login.cs
+++ recp 18 ok/login.cs
@@ -13,23 +13,34 @@ namespace recp_18_ok
 {
     public partial class login : Form
     {
+        pass pass = new pass();
+
         public login()
         {
             InitializeComponent();
         }
 
         private void Logg_Click(object sender, EventArgs e)
-        {
-            const string correctEmail = "najah";
-            const string correctPassword = "123";
-            if (Userbox.Text == correctEmail && passbox.Text == correctPassword)
+        { 
+            pass.managers.Add(new pass { username = "najah", password = "123" });
+            pass.managers.Add(new pass { username = "Vanesa", password = "123" });
+            pass.managers.Add(new pass { username = "Kamal", password = "123" });
+            pass.managers.Add(new pass { username = "Simon", password = "123" });
+            pass.managers.Add(new pass { username = "Kevin", password = "123" });
+            pass.managers.Add(new pass { username = "Cornelia", password = "123" });
+            
+            string username = Userbox.Text.ToLower();
+            string password = passbox.Text;
+
+            if (pass.RightInloggning(username,password))
             {
                 MessageBox.Show("Du är inloggad");
-                //Uppgift 3
+                // instanse of adminform
                 admin admin = new admin();
                 admin.ShowDialog();
-
             }
+            else
+                MessageBox.Show("Invalid username or password");
         }
     }
 }
