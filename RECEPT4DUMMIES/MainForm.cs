9037389bb2138bb792c1adf0d7cb8121561bea35
Obi-Wan-More-Rep <kevin.branger@live.com>
2023-10-24 23:56:07 +0200

    Jag har skrivit upp alla metoder och klasser som jag kan tänka mig vårt program kan ha.

--- RECEPT4DUMMIES/MainForm.cs
+++ RECEPT4DUMMIES/MainForm.cs
@@ -2,9 +2,66 @@ namespace RECEPT4DUMMIES
 {
     public partial class MainForm : Form
     {
+        public List<Recipe> recipes = new List<Recipe>();
+        public List<Admin> admins = new List<Admin>();
+
+        public bool isAdmin;
+
+
         public MainForm()
         {
             InitializeComponent();
         }
+
+        // Lägga till recept i recipe listan och i DataGridView
+        private void LoadRecipes()
+        {
+            // Streamreader för att läsa texfilen och för varje rad skapa en instans av "Recipe" klassen och lägga till den i "recipes" listan
+        }
+
+
+        // Lägga till admins i admins listan
+        private void LoadAdmin()
+        {
+            // Streamreader för att läsa texfilen och för varje rad skapa en instans av "Admin" klassen och lägga till den i "admins" listan
+        }
+
+
+        // Nollställa DataGridView
+        private void ResetDataGridView()
+        {
+            // (namnet på DatagridViewen).Rows.Clear();
+        }
+
+
+        // söka på recept från recipes listan och visa i DataGridView
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Öppna en ny Form när man klickar på en rad för att visa detaljerad information (För tillfället kallar jag Formen för "RecipeDetailsForm"
+        // Metod
+
+
+
+        // Logga in som Admin
+        private void SignInButton_Click(Object sender, EventArgs e)
+        {
+            // Öppna en ny Forms "FormLogIn" där man kan skriva Användarnamn och lösenord
+
+            // Om inloggningen lyckas så ändras "isAdmin" variabeln i fältet på denna klass till "true"
+
+            // Efter att man har loggat in så går man tillbaka till denna Form
+        }
+
+
+        // Ta bort markerad recipe
+        private void DeleteSelectedRecipeButton_Click()
+        {
+            // Ta bort receptet från listan
+
+        }
+
     }
 }
---
+++ RECEPT4DUMMIES/RecipeDetailsForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace RECEPT4DUMMIES
+{
+    public partial class RecipeDetailsForm : Form
+    {
+        public RecipeDetailsForm()
+        {
+            InitializeComponent();
+        }
+
+        // Dubbelklicka på Textrutorna för att kunna ändra dem om du är inloggad som en admin. Om inte så händer inget.
+        private void textbox_DoubleClick(object sender, EventArgs e)
+        {
+            textBox.ReadOnly = !textBox.ReadOnly;
+        }
+
+
+        // Om du klickar på "Enter" på nuvarande textruta så ändras den till "Read Only" och recept objektet updateras i "recipes" listan som finnns i "MainForm" klassen.
+    }
+}
