9be2f943cb9fe98d0a481199a3aab8724b43e581
Obi-Wan-More-Rep <kevin.branger@live.com>
2023-10-29 01:19:16 +0200

    Jag skapade en ny version av mitt test projekt (Test ver 4) som ni kan hitta under "Tillfälliga projekt Kevin".  Programmet kan söka på recept och typer, redigera, lägga till och ta bort recept.

---
+++ RECEPT4DUMMIES Test ver 4/FormLogIn.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace RECEPT4DUMMIES
+{
+    partial class FormLogIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            UsernameTextbox = new TextBox();
+            PasswordTextbox = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            SignInButton = new Button();
+            SuspendLayout();
+            // 
+            // UsernameTextbox
+            // 
+            UsernameTextbox.Location = new Point(380, 126);
+            UsernameTextbox.Name = "UsernameTextbox";
+            UsernameTextbox.Size = new Size(200, 23);
+            UsernameTextbox.TabIndex = 0;
+            // 
+            // PasswordTextbox
+            // 
+            PasswordTextbox.Location = new Point(380, 175);
+            PasswordTextbox.Name = "PasswordTextbox";
+            PasswordTextbox.Size = new Size(200, 23);
+            PasswordTextbox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(245, 129);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Username";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(245, 175);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Password";
+            // 
+            // SignInButton
+            // 
+            SignInButton.Location = new Point(352, 241);
+            SignInButton.Name = "SignInButton";
+            SignInButton.Size = new Size(75, 23);
+            SignInButton.TabIndex = 2;
+            SignInButton.Text = "Sign in";
+            SignInButton.UseVisualStyleBackColor = true;
+            SignInButton.Click += SignInButton_Click;
+            // 
+            // FormLogIn
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(SignInButton);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(PasswordTextbox);
+            Controls.Add(UsernameTextbox);
+            Name = "FormLogIn";
+            Text = "FormLogIn";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox UsernameTextbox;
+        private TextBox PasswordTextbox;
+        private Label label1;
+        private Label label2;
+        private Button SignInButton;
+    }
+}
---
+++ RECEPT4DUMMIES Test ver 4/RecipeDetailsForm.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Diagnostics;
+
+namespace RECEPT4DUMMIES
+{
+    public partial class RecipeDetailsForm : Form
+    {
+        Recipe selectedRecipe;
+        public Recipe UppdatedRecipe { get; set; }
+        private bool isAdminSignedIn { get; set; }
+        public bool DeleteRecipe { get; private set; }
+        public bool AddRecipe { get; private set; }
+
+        public RecipeDetailsForm(Recipe selectedRecipe, bool isAdminSignedIn)
+        {
+            InitializeComponent();
+            
+            this.isAdminSignedIn = isAdminSignedIn;
+            Debug.WriteLine("RecipeDetailsForm constructor");
+            Debug.WriteLine($"isAdminSignedIn: {isAdminSignedIn}");
+
+            // Om du klickar på att lägga till ett nytt recept i MainForm
+            if (selectedRecipe == null)
+            {
+                this.selectedRecipe = new Recipe();
+                AddNewRecipeUserInterfaceSettings();
+            }
+
+
+            // Om du klickar/markerar ett recept i MainForm
+            else
+            {
+                // kopierar selectedRecipe egenskaper så om båda objekten har samma sak så uppdateras inte recept listan in MainForm. Behöver liksom inte ta bort och lägga till recept om man inte har ändrat ett recept med andra ord
+                UppdatedRecipe = new Recipe
+                {
+                    Title = selectedRecipe.Title,
+                    Description = selectedRecipe.Description,
+                    Type = selectedRecipe.Type
+                };
+                this.selectedRecipe = selectedRecipe;
+                StandardUserInterfaceSettings();
+            }
+        }
+
+
+        private void StandardUserInterfaceSettings()
+        {
+            // Visa knappar för redigering om du är Admin
+            if (isAdminSignedIn)
+            {
+                uppdateRecipeButton.Visible = true;
+                DeleteRecipeButton.Visible = true;
+            }
+            Debug.WriteLine("StandardUserInterfaceSettings method");
+            Debug.WriteLine($"isAdminSignedIn: {isAdminSignedIn}");
+
+            // Tar bort Borderstyle för att göra så att textboxarna liknar mera labels i utseendet
+            titleTextbox.BorderStyle = BorderStyle.None;
+            typeTextbox.BorderStyle = BorderStyle.None;
+            descriptionRichTextBox.BorderStyle = BorderStyle.None;
+
+            // Sätta färgen på textboxarna till samma som finns för Form bakgrunden
+            titleTextbox.BackColor = this.BackColor;
+            typeTextbox.BackColor = this.BackColor;
+            descriptionRichTextBox.BackColor = this.BackColor;
+
+            // lägga till receptet i textrutorna
+            titleTextbox.Text = selectedRecipe.Title;
+            typeTextbox.Text = selectedRecipe.Type;
+            //descriptionRichTextBox.Text = selectedRecipe.Description;
+
+            descriptionRichTextBox.Lines = selectedRecipe.Description.Split(@"\n");
+
+
+        }
+
+        private void AddNewRecipeUserInterfaceSettings()
+        {
+            titleTextbox.ReadOnly = false;
+            typeTextbox.ReadOnly = false;
+            descriptionRichTextBox.ReadOnly = false;
+
+            AddRecipeButton.Visible = true;
+
+            // Tar bort Borderstyle för att göra så att textboxarna liknar mera labels i utseendet
+            titleTextbox.BorderStyle = BorderStyle.None;
+            typeTextbox.BorderStyle = BorderStyle.None;
+            descriptionRichTextBox.BorderStyle = BorderStyle.None;
+        }
+
+        // Kanske kunde istället lagt till en knapp som kan heta "Enable Edit mode". Då kan jag ändra bakgrundsfärgen för alla textrutor samtidigt men vad jag har skrivit nedan fungerar med.
+
+        private void titleTextbox_DoubleClick(object sender, EventArgs e)
+        {
+            if (isAdminSignedIn && UppdatedRecipe != null)
+            {
+                titleTextbox.ReadOnly = !titleTextbox.ReadOnly;
+
+                if (titleTextbox.BackColor == this.BackColor)
+                {
+                    titleTextbox.BackColor = Color.White;
+                }
+                else
+                {
+                    titleTextbox.BackColor = this.BackColor;
+                }
+
+
+            }
+
+        }
+
+        private void descriptionRichTextBox_DoubleClick(object sender, EventArgs e)
+        {
+            if (isAdminSignedIn && UppdatedRecipe != null)
+            {
+                descriptionRichTextBox.ReadOnly = !descriptionRichTextBox.ReadOnly;
+
+                if (descriptionRichTextBox.BackColor == this.BackColor)
+                {
+                    descriptionRichTextBox.BackColor = Color.White;
+                }
+                else
+                {
+                    descriptionRichTextBox.BackColor = this.BackColor;
+                }
+            }
+        }
+
+        private void typeTextbox_DoubleClick(object sender, EventArgs e)
+        {
+            if (isAdminSignedIn && UppdatedRecipe != null)
+            {
+                typeTextbox.ReadOnly = !typeTextbox.ReadOnly;
+
+                if (typeTextbox.BackColor == this.BackColor)
+                {
+                    typeTextbox.BackColor = Color.White;
+                }
+                else
+                {
+                    typeTextbox.BackColor = this.BackColor;
+                }
+            }
+
+        }
+
+        private void uppdateRecipeButton_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Update button clicked in RecipeDetailsForm.");
+
+            UppdatedRecipe.Title = titleTextbox.Text;
+            UppdatedRecipe.Type = typeTextbox.Text;
+            UppdatedRecipe.Description = string.Join(@"\n", descriptionRichTextBox.Lines);
+
+            // kan tta bort sen this.close
+            this.Close();
+        }
+
+        private void DeleteRecipeButton_Click(object sender, EventArgs e)
+        {
+            DeleteRecipe = true;
+            this.Close();
+        }
+
+        private void AddRecipeButton_Click(object sender, EventArgs e)
+        {
+            AddRecipe = true;
+            UppdatedRecipe = new Recipe
+            {
+                Title = titleTextbox.Text,
+                Type = typeTextbox.Text,
+                Description = descriptionRichTextBox.Text
+            };
+            this.Close();
+        }
+
+
+
+        // Om du klickar på "Enter" på nuvarande textruta så ändras den till "Read Only" och recept objektet updateras i "recipes" listan som finnns i "MainForm" klassen.
+        //private void textbox_KeyDown(object sender, KeyEventArgs e)
+        //{
+        //    if (e.KeyCode == Keys.Enter)
+        //    {
+        //        textBox.ReadOnly = true;
+        //    }
+        //}
+    }
+}
